8f25b01cd7838c2755ca5af46c6a30d55639f553
Julian Peeters <julianpeeters@gmail.com>
2025-11-10 22:28:23 -0800

    Refactor tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -15,14 +15,10 @@ fn test_public_api() {
   let mut b: HashMap<&Two, &Two> = HashMap::new();
   let mut c: HashMap<&Two, &Two> = HashMap::new();
   let mut d: HashMap<&Two, &Two> = HashMap::new();
-  a.insert(&Two::_1, &Two::_1);
-  a.insert(&Two::_2, &Two::_1);
-  b.insert(&Two::_2, &Two::_2);
-  b.insert(&Two::_1, &Two::_1);
-  c.insert(&Two::_2, &Two::_1);
-  c.insert(&Two::_1, &Two::_2);
-  d.insert(&Two::_2, &Two::_2);
-  d.insert(&Two::_1, &Two::_2);
+  a.insert(&Two::_1, &Two::_1); a.insert(&Two::_2, &Two::_1);
+  b.insert(&Two::_1, &Two::_1); b.insert(&Two::_2, &Two::_2);
+  c.insert(&Two::_1, &Two::_2); c.insert(&Two::_2, &Two::_1);
+  d.insert(&Two::_1, &Two::_2); d.insert(&Two::_2, &Two::_2);
   assert_eq!(Two::VALUES.tetrate(), vec!(a, b, c, d));
 
   let mut e: Relation<&Two, &Two> = Relation::new();
