f00595b1dbcdd7f5baa9e8fc301b19b58b534210
Julian Peeters <julianpeeters@gmail.com>
2025-11-10 22:39:23 -0800

    Back function repr with HashSet

--- src/function/mod.rs
+++ src/function/mod.rs
@@ -1,8 +1,8 @@
 use crate::set::{Zero, One, Two, Three, Four, Five};
 use itertools::Itertools;
-use std::collections::HashMap;
+use std::collections::HashSet;
 
-pub type Function<A, B> = HashMap<A, B>;
+type Function<A, B> = HashSet<(A, B)>;
 
 pub trait Tetration<A> {
   fn tetrate(&self) -> Vec<Function<&A, &A>>;
@@ -12,7 +12,7 @@ impl Tetration<Zero> for [&Zero; 0] {
   fn tetrate(&self) -> Vec<Function<&Zero, &Zero>> {
     self.iter()
         .map(|_| {
-          let functions = Function::new();
+          let functions = HashSet::new();
           functions
         })
         .collect_vec()
@@ -23,8 +23,8 @@ impl Tetration<One> for [&One; 1] {
   fn tetrate(&self) -> Vec<Function<&One, &One>> {
     self.iter()
         .map(|p| {
-          let mut functions = Function::new();
-          functions.insert(&One::_1, *p);
+          let mut functions = HashSet::new();
+          functions.insert((&One::_1, *p));
           functions
         })
         .collect_vec()
@@ -35,9 +35,9 @@ impl Tetration<Two> for [&Two; 2] {
   fn tetrate(&self) -> Vec<Function<&Two, &Two>> {
     self.iter().cartesian_product(self)
         .map(|p| {
-          let mut functions = Function::new();
-          functions.insert(&Two::_1, *p.0);
-          functions.insert(&Two::_2, *p.1);
+          let mut functions = HashSet::new();
+          functions.insert((&Two::_1, *p.0));
+          functions.insert((&Two::_2, *p.1));
           functions
         })
         .collect_vec()
@@ -48,10 +48,10 @@ impl Tetration<Three> for [&Three; 3] {
   fn tetrate(&self) -> Vec<Function<&Three, &Three>> {
     self.iter().cartesian_product(self).cartesian_product(self)
         .map(|p| {
-          let mut functions = Function::new();
-          functions.insert(&Three::_1, *p.0.0);
-          functions.insert(&Three::_2, *p.0.1);
-          functions.insert(&Three::_3, *p.1);
+          let mut functions = HashSet::new();
+          functions.insert((&Three::_1, *p.0.0));
+          functions.insert((&Three::_2, *p.0.1));
+          functions.insert((&Three::_3, *p.1));
           functions
         })
         .collect_vec()
@@ -62,11 +62,11 @@ impl Tetration<Four> for [&Four; 4] {
   fn tetrate(&self) -> Vec<Function<&Four, &Four>> {
     self.iter().cartesian_product(self).cartesian_product(self).cartesian_product(self)
         .map(|p| {
-          let mut functions = Function::new();
-          functions.insert(&Four::_1, *p.0.0.0);
-          functions.insert(&Four::_2, *p.0.0.1);
-          functions.insert(&Four::_3, *p.0.1);
-          functions.insert(&Four::_4, *p.1);
+          let mut functions = HashSet::new();
+          functions.insert((&Four::_1, *p.0.0.0));
+          functions.insert((&Four::_2, *p.0.0.1));
+          functions.insert((&Four::_3, *p.0.1));
+          functions.insert((&Four::_4, *p.1));
           functions
         })
         .collect_vec()
@@ -77,12 +77,12 @@ impl Tetration<Five> for [&Five; 5] {
   fn tetrate(&self) -> Vec<Function<&Five, &Five>> {
     self.iter().cartesian_product(self).cartesian_product(self).cartesian_product(self).cartesian_product(self)
         .map(|p| {
-          let mut functions = Function::new();
-          functions.insert(&Five::_1, *p.0.0.0.0);
-          functions.insert(&Five::_2, *p.0.0.0.1);
-          functions.insert(&Five::_3, *p.0.0.1);
-          functions.insert(&Five::_4, *p.0.1);
-          functions.insert(&Five::_5, *p.1);
+          let mut functions = HashSet::new();
+          functions.insert((&Five::_1, *p.0.0.0.0));
+          functions.insert((&Five::_2, *p.0.0.0.1));
+          functions.insert((&Five::_3, *p.0.0.1));
+          functions.insert((&Five::_4, *p.0.1));
+          functions.insert((&Five::_5, *p.1));
           functions
         })
         .collect_vec()
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 use sketches::{
   function::Tetration,
@@ -11,14 +11,14 @@ fn test_public_api() {
 
   assert_eq!(Two::VALUES, [&Two::_1, &Two::_2]);
 
-  let mut a: HashMap<&Two, &Two> = HashMap::new();
-  let mut b: HashMap<&Two, &Two> = HashMap::new();
-  let mut c: HashMap<&Two, &Two> = HashMap::new();
-  let mut d: HashMap<&Two, &Two> = HashMap::new();
-  a.insert(&Two::_1, &Two::_1); a.insert(&Two::_2, &Two::_1);
-  b.insert(&Two::_1, &Two::_1); b.insert(&Two::_2, &Two::_2);
-  c.insert(&Two::_1, &Two::_2); c.insert(&Two::_2, &Two::_1);
-  d.insert(&Two::_1, &Two::_2); d.insert(&Two::_2, &Two::_2);
+  let mut a: HashSet<(&Two, &Two)> = HashSet::new();
+  let mut b: HashSet<(&Two, &Two)> = HashSet::new();
+  let mut c: HashSet<(&Two, &Two)> = HashSet::new();
+  let mut d: HashSet<(&Two, &Two)> = HashSet::new();
+  a.insert((&Two::_1, &Two::_1)); a.insert((&Two::_2, &Two::_1));
+  b.insert((&Two::_1, &Two::_1)); b.insert((&Two::_2, &Two::_2));
+  c.insert((&Two::_1, &Two::_2)); c.insert((&Two::_2, &Two::_1));
+  d.insert((&Two::_1, &Two::_2)); d.insert((&Two::_2, &Two::_2));
   assert_eq!(Two::VALUES.tetrate(), vec!(a, b, c, d));
 
   let mut e: Relation<&Two, &Two> = Relation::new();
