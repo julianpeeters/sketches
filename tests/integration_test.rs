f7371024c0cbfbb705e8942afca5391560f49353
Julian Peeters <julianpeeters@gmail.com>
2025-11-10 20:13:39 -0800

    Reformat tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,26 +1,26 @@
 use std::collections::HashMap;
 
 use sketches::{
-    set::Two, tetration::Tetration
+  set::Two, tetration::Tetration
 };
 
 #[test]
 fn test_public_api() {
 
-    assert_eq!(Two::VALUES, [&Two::_1, &Two::_2]);
+  assert_eq!(Two::VALUES, [&Two::_1, &Two::_2]);
 
-    let mut a: HashMap<&Two, &Two> = HashMap::new();
-    let mut b: HashMap<&Two, &Two> = HashMap::new();
-    let mut c: HashMap<&Two, &Two> = HashMap::new();
-    let mut d: HashMap<&Two, &Two> = HashMap::new();
-    a.insert(&Two::_1, &Two::_1);
-    a.insert(&Two::_2, &Two::_1);
-    b.insert(&Two::_2, &Two::_2);
-    b.insert(&Two::_1, &Two::_1);
-    c.insert(&Two::_2, &Two::_1);
-    c.insert(&Two::_1, &Two::_2);
-    d.insert(&Two::_2, &Two::_2);
-    d.insert(&Two::_1, &Two::_2);
-    assert_eq!(Two::VALUES.tetrate(), vec!(a, b, c, d));
+  let mut a: HashMap<&Two, &Two> = HashMap::new();
+  let mut b: HashMap<&Two, &Two> = HashMap::new();
+  let mut c: HashMap<&Two, &Two> = HashMap::new();
+  let mut d: HashMap<&Two, &Two> = HashMap::new();
+  a.insert(&Two::_1, &Two::_1);
+  a.insert(&Two::_2, &Two::_1);
+  b.insert(&Two::_2, &Two::_2);
+  b.insert(&Two::_1, &Two::_1);
+  c.insert(&Two::_2, &Two::_1);
+  c.insert(&Two::_1, &Two::_2);
+  d.insert(&Two::_2, &Two::_2);
+  d.insert(&Two::_1, &Two::_2);
+  assert_eq!(Two::VALUES.tetrate(), vec!(a, b, c, d));
 
 }
